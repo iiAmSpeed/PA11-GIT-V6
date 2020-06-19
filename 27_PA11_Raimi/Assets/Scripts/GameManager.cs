88483fa9035e258ecb708b71dcd465ac30b74b6d
iiAmSpeed <raiminewboyz@gmail.com>
2020-06-19 14:38:32 +0800

    Updated PA11

---
+++ 27_PA11_Raimi/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Text scoretxt;
+    private int Score = 0;
+
+    public static GameManager gameManager;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+     if(gameManager == null)
+        {
+            gameManager = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScore(int score)
+    {
+        Score += score;
+        scoretxt.text = "Score: " + Score;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void GameOver()
+    {
+
+        SceneManager.LoadScene("GameOver");
+         
+    }
+}
---
+++ 27_PA11_Raimi/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public List<GameObject> Cubes;
+
+    float PositionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+
+    }
+
+    void SpawnObjects()
+    {
+        int CubeToSpawn = Random.Range(0, 2);
+
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        Instantiate(Cubes[CubeToSpawn], transform.position, transform.rotation);
+    }
+}
